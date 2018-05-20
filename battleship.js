1e0139ed5f55cd83b90f9f8883e8132bda41fee1
EssonXu <46305542@qq.com>
2018-05-20 15:29:30 +0800

    实现displayHit和displayMiss

--- battleship.js
+++ battleship.js
@@ -5,12 +5,14 @@ var view =/*实现view对象*/
         var messageArea = document.getElementById("messageArea");/*获取网页中的元素messageAreaS*/
         messageArea.innerHTML = msg;/*将元素messageArea的innerHTML设置为msg,以更新该元素的文本*/
     },
-    displayHit:function(location)
+    displayHit:function(location)/*别忘了，location是根据行号和列号生成的，它是一个<td>元素的id*/
     {
-
+        var cell = document.getElementById(location);/*使用根据玩家猜测生成的id来获取要更新的元素*/
+        cell.setAttribute("class","hit"); /*接下来，我们将这个<td>元素的class设置为hit。这将立即在这个元素中显示战舰图像*/
     },
     displayMiss:function(location)
     {
-        
+        var cell = document.getElementById(location)
+        cell.setAttribute("class","miss");/*在displayMiss中如法炮制，但将class特性设置为miss，在元素中显示MISS图像*/
     }
 }
